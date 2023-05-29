c9ddcbf02326de86a1e13cdc2addb1ca29961af5
Raymond <73725736+raymondanythings@users.noreply.github.com>
2023-05-29 13:17:59 +0900

    배포를 위한 웹뷰 설치

--- App.tsx
+++ App.tsx
@@ -9,11 +9,13 @@ import Router from './router'
 import { NavigationContainer } from '@react-navigation/native'
 import DeviceInfo from 'react-native-device-info'
 import asyncStorage from '@react-native-async-storage/async-storage'
+import WebView from 'react-native-webview'
 function App(): JSX.Element {
   return (
-    <NavigationContainer>
-      <Router />
-    </NavigationContainer>
+    <WebView source={{ uri: 'https://client-eta-rust.vercel.app/' }} />
+    // <NavigationContainer>
+    //   <Router />
+    // </NavigationContainer>
   )
 }
 
