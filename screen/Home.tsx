243183fe93e81351d341d3b35f0d856e6a2f3093
yunna <homer_j@hanmail.net>
2023-05-30 19:06:29 +0900

    design : 1차 와이어프레임 홈화면 UI

--- screen/Home.tsx
+++ screen/Home.tsx
@@ -1,6 +1,6 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import React from 'react'
-import { Text, TouchableOpacity } from 'react-native'
+import { Pressable, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 const Home = ({ navigation }: { navigation: any }) => {
@@ -8,18 +8,67 @@ const Home = ({ navigation }: { navigation: any }) => {
   //   await AsyncStorage.getItem('')
   // }
   return (
-    <SafeAreaView>
-      <Text>
-        Home
-        <TouchableOpacity onPress={() => navigation.push('onboard')}>
-          <Text>다음</Text>
-        </TouchableOpacity>
+    <View style={styles.container}>
+      <SafeAreaView style={styles.safeAreaContainer}>
+        <Text style={styles.heading}>달리는 포도</Text>
+        <Text style={styles.subheading}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua.
+        </Text>
+        <View style={styles.characterContainer}>
+          <Text style={styles.heading}>메인 캐릭터</Text>
+        </View>
+        <Pressable style={styles.nextButton} onPress={() => navigation.push('onboard')}>
+          <Text style={styles.btnInnerText}>서비스 이용하기</Text>
+        </Pressable>
         <TouchableOpacity onPress={async () => await AsyncStorage.clear()}>
           <Text>스토리지 지우기</Text>
         </TouchableOpacity>
-      </Text>
-    </SafeAreaView>
+      </SafeAreaView>
+    </View>
   )
 }
 
 export default Home
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  safeAreaContainer: {
+    alignItems: 'center',
+    paddingHorizontal: 40,
+  },
+  heading: {
+    fontSize: 28,
+    fontWeight: '700',
+    marginBottom: 8,
+  },
+  subheading: {
+    fontSize: 14,
+    fontWeight: '400',
+    marginBottom: 55,
+  },
+  characterContainer: {
+    borderColor: '#C2D1D9',
+    backgroundColor: '#C2D1D9',
+    width: 263,
+    height: 300,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 35,
+  },
+  nextButton: {
+    paddingHorizontal: 80,
+    paddingVertical: 10,
+    backgroundColor: '#4E4E4E',
+    borderRadius: 20,
+    width: 263,
+    alignItems: 'center',
+    marginTop: 20,
+    marginBottom: 9,
+  },
+  btnInnerText: { fontSize: 12, color: 'white', fontWeight: '400' },
+})
