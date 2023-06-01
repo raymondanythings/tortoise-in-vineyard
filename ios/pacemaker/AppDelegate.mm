047680566d8cc5edd2993b90b109f43b68976cc2
yunna <homer_j@hanmail.net>
2023-06-01 18:18:52 +0900

    chore : Google Maps iOS 설치

--- ios/pacemaker/AppDelegate.mm
+++ ios/pacemaker/AppDelegate.mm
@@ -2,11 +2,15 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import "RCTAppleHealthKit.h"
+#import <GoogleMaps/GoogleMaps.h>
+
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyCKCM33Kcfmo-fHNylOkOSuwiVGiwtDWvY"]; // add this line using the api key obtained from Google Console
+
   self.moduleName = @"pacemaker";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
