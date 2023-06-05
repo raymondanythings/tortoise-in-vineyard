666416b4aef9cb16c620fc9614db20b97fc5950a
yunna <homer_j@hanmail.net>
2023-06-05 21:07:21 +0900

    feature : 포도알 애니메이션 추가

--- app/screen/Complete.tsx
+++ app/screen/Complete.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react'
-import { View, Image } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { View, Image, Animated } from 'react-native'
 import Text from '../components/Text'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import globalStyle from '../common/globalStyle'
@@ -11,6 +11,21 @@ import bigEmotion from '../constants/bigEmotion'
 
 const Complete = () => {
   const navigation = useNavigation()
+  const [animation, setAnimation] = useState(new Animated.Value(0))
+  useEffect(() => {
+    Animated.spring(animation, {
+      toValue: 1,
+      tension: 20,
+      friction: 2,
+      useNativeDriver: true,
+    }).start()
+  }, [])
+
+  const bounceAnimation = animation.interpolate({
+    inputRange: [0, 0.5, 1],
+    outputRange: [0, -150, 0],
+  })
+
   // 3초 뒤에 다른 페이지로 이동
   // 다음 페이지가 현재 없어서 home으로 가게 해둠
   useEffect(() => {
@@ -29,14 +44,24 @@ const Complete = () => {
       </View>
       <View style={[{ flex: 20, justifyContent: 'center', alignItems: 'center' }]}>
         <Confetti />
-        <Image
-          style={{ position: 'absolute', width: 214, height: 214 }}
-          source={Icon.GRAPEFORCONFETTI}
-        />
-        <Image
-          source={bigEmotion.PROUD}
-          style={{ position: 'absolute', width: 214, height: 214 }}
-        />
+        <Animated.View
+          style={{
+            position: 'absolute',
+            backgroundColor: 'yellow',
+            justifyContent: 'center',
+            alignItems: 'center',
+            transform: [{ translateY: bounceAnimation }],
+          }}
+        >
+          <Image
+            style={{ position: 'absolute', width: 214, height: 214 }}
+            source={Icon.GRAPEFORCONFETTI}
+          />
+          <Image
+            source={bigEmotion.PROUD}
+            style={{ position: 'absolute', width: 214, height: 214 }}
+          />
+        </Animated.View>
       </View>
       <View style={[globalStyle.fullWidth, globalStyle.footer]}></View>
     </SafeAreaView>
