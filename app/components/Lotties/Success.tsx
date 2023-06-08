c01940cb169182486e213816cc5598a4153a129e
yunna <homer_j@hanmail.net>
2023-06-09 06:24:29 +0900

    fix : 디자인 수정사항 반영

--- app/components/Lotties/Success.tsx
+++ app/components/Lotties/Success.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { View } from 'react-native'
 import LottieView from 'lottie-react-native'
-import success from '../../assets/lotties/success.json'
+import checkPink from '../../assets/lotties/checkPink.json'
 
 const Failure = () => {
   return (
     <View style={{ flex: 1, height: '100%', alignItems: 'center', justifyContent: 'center' }}>
       <LottieView
         style={{ width: '100%', height: '100%' }}
-        source={success}
+        source={checkPink}
         autoPlay
         loop={false}
       />
